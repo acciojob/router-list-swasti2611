59262b7cfc88faf3f4ec678f511a21a5b8ce9641
Swati jagtap <swatijagtap5060@gmail.com>
2024-03-05 14:34:13 +0530

    Submitted

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 const ItemDetail = () => {
     let {id}=useParams();
+    // let
   return (
     <div>
+
          <h1>Item {id}</h1>
       <p>Description for Item {id}</p>
     </div>
